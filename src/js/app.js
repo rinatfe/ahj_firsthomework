f26fdb7410795d7e712dbe6b01aade6b2cbf7361
Rinat Kamaletdinov <sky2208@ya.ru>
2021-05-12 22:22:34 +0400

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 // TODO: write code here
 
 // comment this to pass build
+/*eslint-disable */
 const unusedVariable = 'variable';
+ /* eslint-enable */
 
 // for demonstration purpose only
 export default function demo(value) {
